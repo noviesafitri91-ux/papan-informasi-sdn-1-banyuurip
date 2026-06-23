1957d0ef4c35d6b72d1a04a5f00e6b9f819930e9
noviesafitri91-ux <noviesafitri91@guru.sd.belajar.id>
2026-06-23 16:31:43 +0700

    script.js

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  loadData();
+  document.getElementById("bg").src = "https://via.placeholder.com/1920x1080";
+
+  document.getElementById("runningText").innerHTML =
+    "<span>TES RUNNING TEXT BERJALAN</span>";
 });
-
-async function loadData() {
-  try {
-    const res = await fetch("https://script.google.com/macros/s/AKfycbz7DyIPNVTXBGPxfU5hToNXiRsgyLnlsszZGt647vgqmBSWXu1Oiv0AfBXJ9FpHyBW3Lw/exec");
-    const data = await res.json();
-
-    console.log("DATA:", data);
-
-    const aktif = data.find(item => item.aktif === "YA");
-
-    if (aktif) {
-
-      // 🔥 GAMBAR (DIPAKSA LOAD ULANG BIAR TIDAK HITAM)
-      const img = document.getElementById("bg");
-      img.src = "";
-      setTimeout(() => {
-        img.src = aktif.gambar;
-      }, 100);
-
-      // 🔥 RUNNING TEXT
-      document.getElementById("runningText").innerHTML =
-        "<span>" + aktif.teks + "</span>";
-
-    } else {
-      document.getElementById("runningText").innerText = "TIDAK ADA DATA AKTIF";
-    }
-
-  } catch (err) {
-    console.error("ERROR:", err);
-    document.getElementById("runningText").innerText = "GAGAL LOAD DATA";
-  }
-}
