7c091a24eaff3b2fef8d10a7f67623950b02a040
noviesafitri91-ux <noviesafitri91@guru.sd.belajar.id>
2026-06-23 16:36:32 +0700

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("bg").src = "https://via.placeholder.com/1920x1080";
-
-  document.getElementById("runningText").innerHTML =
-    "<span>TES RUNNING TEXT BERJALAN</span>";
+  loadData();
 });
+
+async function loadData() {
+  try {
+    const res = await fetch("https://script.google.com/macros/s/AKfycbz7DyIPNVTXBGPxfU5hToNXiRsgyLnlsszZGt647vgqmBSWXu1Oiv0AfBXJ9FpHyBW3Lw/exec");
+    const data = await res.json();
+
+    const aktif = data.find(item => item.aktif === "YA");
+
+    if (aktif) {
+
+      // GAMBAR
+      document.getElementById("bg").src = aktif.gambar;
+
+      // RUNNING TEXT (INI YANG BENAR)
+      document.getElementById("runningText").innerHTML =
+        "<span>" + aktif.teks + "</span>";
+
+    }
+
+  } catch (err) {
+    console.log(err);
+  }
+}
